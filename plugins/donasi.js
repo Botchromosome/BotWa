e9c37e9c2ebd00102558bc642df5944c51220578
Botchromosome <91787098+Botchromosome@users.noreply.github.com>
2021-10-06 16:25:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
+│ • XL [081804832728]
+│ • Telkomsel [081318759394]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
